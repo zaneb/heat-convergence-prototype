7c5d09cce141cd22661fe8c49222665e15a9c82a
Zane Bitter <zbitter@redhat.com>
2014-10-16 15:44:56 -0400

    Create a basic in-memory datastore
    The datastores are based on named tuples, to make changing the schema as
    simple as possible while still actually verifying it.

---
+++ converge/framework/datastore.py
@@ -0,0 +1,39 @@
+import collections
+import copy
+import itertools
+import logging
+
+
+logger = logging.getLogger('store')
+
+
+class Datastore(object):
+    def __init__(self, name, *fields):
+        self.name = name
+        self._store = {}
+        self.ids = itertools.count()
+        self.DataType = collections.namedtuple(name, fields)
+
+    def create_with_key(self, key, **kwargs):
+        logger.debug('[%s] Creating %s' % (self.name, key))
+        self._store[key] = self.DataType(key, **copy.deepcopy(kwargs))
+        return key
+
+    def create(self, **kwargs):
+        return self.create_with_key(next(self.ids), **kwargs)
+
+    def read(self, key):
+        logger.debug('[%s] Reading %s' % (self.name, key))
+        return copy.deepcopy(self._store[key])
+
+    def update(self, key, **kwargs):
+        if isinstance(key, self.DataType):
+            key = key[0]
+        logger.debug('[%s] Updating %s' % (self.name, key))
+        self._store[key] = self._store[key]._replace(**copy.deepcopy(kwargs))
+
+    def delete(self, key):
+        if isinstance(key, self.DataType):
+            key = key[0]
+        logger.debug('[%s] Deleting %s' % (self.name, key))
+        del self._store[key]
