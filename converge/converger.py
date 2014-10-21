ba41d3a673f47a15e5be47d09692ef7f2afe7738
Zane Bitter <zbitter@redhat.com>
2014-10-21 15:45:37 -0400

    Follow dependency graph
    After checking a particular resource, trigger checks on dependent
    resources according to the dependency graph. This means we will create
    all resources in dependency order. Yay!
    
    To handle the case where a resource is dependent on multiple ancestors,
    introduce a datastore purely for counting the dependencies satisfied.
    This reduces the lock contention to the minimum possible domain.

--- converge/converger.py
+++ converge/converger.py
@@ -1,6 +1,12 @@
+from .framework import datastore
 from .framework import process
 
 from . import resource
+from . import template
+
+
+sync_points = datastore.Datastore('SyncPoint',
+                                  'key', 'predecessors', 'satisfied')
 
 
 class Converger(process.MessageProcessor):
@@ -10,5 +16,37 @@ class Converger(process.MessageProcessor):
     @process.asynchronous
     def check_resource(self, resource_key, template_key):
         rsrc = resource.Resource.load(resource_key)
+        tmpl = template.Template.load(template_key)
+
         if rsrc.physical_resource_id is None:
             rsrc.create()
+
+        for req in rsrc.requirers:
+            req_rsrc = resource.Resource.load(req)
+            predecessors = tmpl.resources[req_rsrc.name].dependency_names()
+            self.propagate_check_resource(req, template_key,
+                                          set(predecessors), rsrc.name)
+
+    def propagate_check_resource(self, next_resource_key, template_key,
+                                 predecessors, sender):
+        if len(predecessors) == 1:
+            # Cut to the chase
+            self.check_resource(next_resource_key, template_key)
+            return
+
+        key = '%d-%d' % (next_resource_key, template_key)
+        try:
+            sync_point = sync_points.read(key)
+        except KeyError:
+            sync_points.create_with_key(key, predecessors=predecessors,
+                                        satisfied=[sender])
+        else:
+            satisfied = sync_point.satisfied + [sender]
+            predecessors |= sync_point.predecessors
+            if set(satisfied).issuperset(predecessors):
+                self.check_resource(next_resource_key, template_key)
+                sync_points.delete(key)
+            else:
+                # Note: update must be atomic
+                sync_points.update(key, predecessors=predecessors,
+                                   satisfied=satisfied)
