10eb543160c746b53b575153e60ecf8fee9b54c4
Zane Bitter <zbitter@redhat.com>
2014-10-28 11:24:59 -0400

    Implement adding resources via updates
    Allow the template to be updated to add resources. Modification of existing
    resources and cleanup of deleted resources is not yet implemented.

--- converge/stack.py
+++ converge/stack.py
@@ -74,3 +74,65 @@ class Stack(object):
         for rsrc_name in deps.leaves():
             processes.converger.check_resource(resources[rsrc_name].key,
                                                self.tmpl.key)
+
+    def _calc_dependencies(self, tmpl_deps, resources):
+        def make_graph_key(rsrc_name):
+            return resources[rsrc_name].graph_key()
+
+        deps = tmpl_deps.translate(make_graph_key)
+
+        reverse_key = lambda gk: gk._replace(forward=False)
+
+        for edge in self.data['deps']:
+            src, targ = edge
+
+            # Walk existing graph in reverse to clean up
+            if targ is not None and targ.forward and src.forward:
+                deps += (reverse_key(targ), reverse_key(src))
+
+            # Keep existing clean up dependencies
+            elif not src.forward and (targ is None or not targ.forward):
+                deps += edge
+
+            # Clean up only after handling the current resources
+            for node in edge:
+                if (node is not None and
+                        node.forward and node.name in resources):
+                    deps += (reverse_key(node), make_graph_key(node.name))
+
+        return deps
+
+    def update(self, tmpl):
+        old_tmpl, self.tmpl = self.tmpl, tmpl
+        self.data['tmpl_key'] = tmpl.key
+
+        logger.info('[%s(%d)] Updating...' % (self.data['name'], self.key))
+
+        current_graph = self.dependencies().graph()
+        rsrcs = {r.name: r for r in resource.Resource.load_all_from_stack(self)
+                           if r.graph_key() in current_graph}
+
+
+        def store_resource(rsrc_name):
+            rsrc = resource.Resource(rsrc_name, self, definitions[rsrc_name])
+            rsrc.store()
+            return rsrc
+
+        definitions = self.tmpl.resources
+        tmpl_deps = self.tmpl.dependencies()
+
+        for rsrc_name in tmpl_deps:
+            if rsrc_name not in rsrcs:
+                rsrcs[rsrc_name] = store_resource(rsrc_name)
+
+        deps = self._calc_dependencies(tmpl_deps, rsrcs)
+        list(deps) # Check for circular dependencies
+        self.data['deps'] = tuple(deps.graph().edges())
+
+        self.store()
+
+        from . import processes
+        for graph_key in deps.leaves():
+            if graph_key.forward:
+                processes.converger.check_resource(graph_key.key,
+                                                   self.tmpl.key)
---
+++ scenarios/update_add.py
@@ -0,0 +1,19 @@
+example_template = Template({
+    'A': RsrcDef({}, []),
+    'B': RsrcDef({}, []),
+    'C': RsrcDef({'a': '4alpha'}, ['A', 'B']),
+    'D': RsrcDef({'c': GetRes('C')}, []),
+    'E': RsrcDef({'ca': GetAtt('C', 'a')}, []),
+})
+engine.create_stack('foo', example_template)
+engine.noop(5)
+
+example_template2 = Template({
+    'A': RsrcDef({}, []),
+    'B': RsrcDef({}, []),
+    'C': RsrcDef({'a': '4alpha'}, ['A', 'B']),
+    'D': RsrcDef({'c': GetRes('C')}, []),
+    'E': RsrcDef({'ca': GetAtt('C', 'a')}, []),
+    'F': RsrcDef({}, ['D', 'E']),
+})
+engine.update_stack('foo', example_template2)
