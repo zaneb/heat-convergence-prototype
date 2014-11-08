9cda685aa25c6f1eae0f0a024427139e4a09f2fb
Zane Bitter <zbitter@redhat.com>
2014-11-08 09:53:35 +0100

    Unify stack create and update
    Create is just a special case of update, and can use the same code.

--- converge/stack.py
+++ converge/stack.py
@@ -46,33 +46,17 @@ class Stack(object):
 
     def create(self):
         self.store()
-
-        logger.info('[%s(%d)] Created' % (self.data['name'], self.key))
-
-        definitions = self.tmpl.resources
-        deps = self.tmpl.dependencies()
-        logger.debug('[%s(%d)] Dependencies: %s' % (self.data['name'],
-                                                    self.key, deps.graph()))
-
-        resources = {}
-        for rsrc_name in reversed(deps):
-            requirers = [resources[r].key for r in deps.required_by(rsrc_name)]
-            rsrc = resource.Resource(rsrc_name, self, definitions[rsrc_name],
-                                     self.tmpl.key, set(requirers))
-            rsrc.store()
-            resources[rsrc_name] = rsrc
-
-        from . import processes
-        for rsrc_name in deps.leaves():
-            processes.converger.check_resource(resources[rsrc_name].key,
-                                               self.tmpl.key)
+        self._create_or_update()
 
     def update(self, tmpl):
         old_tmpl, self.tmpl = self.tmpl, tmpl
         self.data['tmpl_key'] = tmpl.key
-        self.store()
 
         logger.info('[%s(%d)] Updating...' % (self.data['name'], self.key))
+        self._create_or_update(old_tmpl.key)
+
+    def _create_or_update(self, current_tmpl_key=None):
+        self.store()
 
         definitions = self.tmpl.resources
         tmpl_deps = self.tmpl.dependencies()
@@ -81,7 +65,7 @@ class Stack(object):
                                                     tmpl_deps.graph()))
 
         rsrcs = {r.name: r for r in resource.Resource.load_all_from_stack(self)
-                           if r.template_key == old_tmpl.key}
+                           if r.template_key == current_tmpl_key}
 
         def store_resource(name):
             requirers = [rsrcs[r].key for r in tmpl_deps.required_by(name)]
