8349ef997ebef59e1eadc86752c33d495656d505
Michal Dulko <michal.dulko@intel.com>
2014-11-24 16:44:16 +0100

    Adapted additional test from Zane's repo and added one testing real update_replace.
    Signed-off-by: Michal Dulko <michal.dulko@intel.com>

---
+++ scenarios/update_remove.py
@@ -0,0 +1,106 @@
+def validate_create():
+    res_ds = get_datastore("Resource")
+    test.testcase.assertEqual(
+        dict(res_ds.dump()),
+        {0: {'key': 0,
+             'name': 'A',
+             'phys_id': 'A_phys_id',
+             'properties': {},
+             'state': 'COMPLETE',
+             'version': 1,
+             'prop_refs': [],
+             },
+         1: {'key': 1,
+             'name': 'B',
+             'phys_id': 'B_phys_id',
+             'properties': {},
+             'state': 'COMPLETE',
+             'version': 1,
+             'prop_refs': [],
+             },
+         2: {'key': 2,
+             'name': 'C',
+             'phys_id': 'C_phys_id',
+             'properties': {'a': '4alpha'},
+             'state': 'COMPLETE',
+             'version': 1,
+             'prop_refs': [],
+             },
+         3: {'key': 3,
+             'name': 'E',
+             'phys_id': 'E_phys_id',
+             'properties': {'ca': '4alpha'},
+             'state': 'COMPLETE',
+             'version': 1,
+             'prop_refs': [{'resource': 'C', 'version': 1}],
+             },
+         4: {'key': 4,
+             'name': 'D',
+             'phys_id': 'D_phys_id',
+             'properties': {'c': 'C_phys_id'},
+             'state': 'COMPLETE',
+             'version': 1,
+             'prop_refs': [],
+             }}
+    )
+
+
+def validate_update():
+    res_ds = get_datastore("Resource")
+    test.testcase.assertEqual(
+        dict(res_ds.dump()),
+        {0: {'key': 0,
+             'name': 'A',
+             'phys_id': 'A_phys_id',
+             'properties': {},
+             'state': 'COMPLETE',
+             'version': 1,
+             'prop_refs': [],
+             },
+         1: {'key': 1,
+             'name': 'B',
+             'phys_id': 'B_phys_id',
+             'properties': {},
+             'state': 'COMPLETE',
+             'version': 1,
+             'prop_refs': [],
+             },
+         2: {'key': 2,
+             'name': 'C',
+             'phys_id': 'C_phys_id',
+             'propertiels': {'a': '4alpha'},
+             'state': 'COMPLETE',
+             'version': 1,
+             'prop_refs': [],
+             },
+         4: {'key': 4,
+             'name': 'D',
+             'phys_id': 'D_phys_id',
+             'properties': {'c': 'C_phys_id'},
+             'state': 'COMPLETE',
+             'version': 1,
+             'prop_refs': [],
+             }}
+    )
+
+example_template = Template({
+    'A': RsrcDef({}, []),
+    'B': RsrcDef({}, []),
+    'C': RsrcDef({'a': '4alpha'}, ['A', 'B']),
+    'D': RsrcDef({'c': GetRes('C')}, ['C']),
+    'E': RsrcDef({'ca': GetAtt('C', 'a')}, ['C']),
+})
+engine.create_stack('foo', example_template)
+engine.converge('foo', 10)
+engine.validate(validate_create)
+
+example_template2 = Template({
+    'A': RsrcDef({}, []),
+    'B': RsrcDef({}, []),
+    'C': RsrcDef({'a': '4alpha'}, ['A', 'B']),
+    'D': RsrcDef({'c': GetRes('C')}, ['C']),
+})
+engine.update_stack('foo', example_template2)
+engine.converge('foo', 10)
+engine.validate(validate_update)
+engine.delete_stack('foo')
---
+++ scenarios/update_replace2.py
@@ -0,0 +1,118 @@
+def validate_create():
+    res_ds = get_datastore("Resource")
+    test.testcase.assertEqual(
+        dict(res_ds.dump()),
+        {0: {'key': 0,
+             'name': 'A',
+             'phys_id': 'A_phys_id',
+             'properties': {'a': 'initial'},
+             'state': 'COMPLETE',
+             'version': 1,
+             'prop_refs': [],
+             },
+         1: {'key': 1,
+             'name': 'B',
+             'phys_id': 'B_phys_id',
+             'properties': {},
+             'state': 'COMPLETE',
+             'version': 1,
+             'prop_refs': [],
+             },
+         2: {'key': 2,
+             'name': 'C',
+             'phys_id': 'C_phys_id',
+             'properties': {'!a': 'initial'},
+             'state': 'COMPLETE',
+             'version': 1,
+             'prop_refs': [{'resource': 'A', 'version': 1}],
+             },
+         3: {'key': 3,
+             'name': 'E',
+             'phys_id': 'E_phys_id',
+             'properties': {'ca': 'initial'},
+             'state': 'COMPLETE',
+             'version': 1,
+             'prop_refs': [{'resource': 'C', 'version': 1}],
+             },
+         4: {'key': 4,
+             'name': 'D',
+             'phys_id': 'D_phys_id',
+             'properties': {'c': 'C_phys_id'},
+             'state': 'COMPLETE',
+             'version': 1,
+             'prop_refs': [],
+             }}
+    )
+
+
+def validate_update():
+    res_ds = get_datastore("Resource")
+    print res_ds.dump()
+    test.testcase.assertEqual(
+        dict(res_ds.dump()),
+        {0: {'key': 0,
+             'name': 'A',
+             'phys_id': 'A_phys_id',
+             'properties': {'a': 'initial'},
+             'state': 'COMPLETE',
+             'version': 1,
+             'prop_refs': [],
+             },
+         1: {'key': 1,
+             'name': 'B',
+             'phys_id': 'B_phys_id',
+             'properties': {},
+             'state': 'COMPLETE',
+             'version': 1,
+             'prop_refs': [],
+             },
+         2: {'key': 2,
+             'name': 'C2',
+             'phys_id': 'C_phys_id',
+             'properties': {'!a': 'initial'},
+             'state': 'COMPLETE',
+             'version': 1,
+             'prop_refs': [{'resource': 'A', 'version': 1}],
+             },
+         3: {'key': 3,
+             'name': 'E',
+             'phys_id': 'E_phys_id',
+             'properties': {'ca': 'initial'},
+             'state': 'COMPLETE',
+             'version': 1,
+             'prop_refs': [{'resource': 'C', 'version': 1}],
+             },
+         4: {'key': 4,
+             'name': 'D',
+             'phys_id': 'D_phys_id',
+             'properties': {'c': 'C_phys_id'},
+             'state': 'COMPLETE',
+             'version': 1,
+             'prop_refs': [],
+             }}
+    )
+
+
+example_template = Template({
+    'A': RsrcDef({'a': 'initial'}, []),
+    'B': RsrcDef({}, []),
+    'C': RsrcDef({'!a': GetAtt('A', 'a')}, ['B', 'A']),
+    'D': RsrcDef({'c': GetRes('C')}, ['C']),
+    'E': RsrcDef({'ca': GetAtt('C', '!a')}, ['C']),
+})
+engine.create_stack('update_replace', example_template)
+engine.converge('update_replace', 10)
+engine.validate(validate_create)
+
+example_template_replaced = Template({
+    'A': RsrcDef({'a': 'updated'}, []),
+    'B': RsrcDef({}, []),
+    'C2': RsrcDef({'!a': GetAtt('A', 'a')}, ['B', 'A']),
+    'D': RsrcDef({'c': GetRes('C')}, ['C']),
+    'E': RsrcDef({'ca': GetAtt('C', '!a')}, ['C']),
+})
+engine.update_stack('update_replace', example_template_replaced)
+engine.converge('update_replace', 10)
+
+engine.validate(validate_update)
+engine.delete_stack('update_replace')
