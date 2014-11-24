9f137c1e07ea3b1bbbc887cbf1169fb06fc6e627
Zane Bitter <zbitter@redhat.com>
2014-11-24 13:28:22 -0500

    Add additional tests from distributed-graph branch

---
+++ scenarios/update_remove.py
@@ -0,0 +1,20 @@
+example_template = Template({
+    'A': RsrcDef({}, []),
+    'B': RsrcDef({}, []),
+    'C': RsrcDef({'a': '4alpha'}, ['A', 'B']),
+    'D': RsrcDef({'c': GetRes('C')}, []),
+    'E': RsrcDef({'ca': GetAtt('C', 'a')}, []),
+})
+engine.create_stack('foo', example_template)
+engine.noop(5)
+engine.call(verify, example_template)
+
+example_template2 = Template({
+    'A': RsrcDef({}, []),
+    'B': RsrcDef({}, []),
+    'C': RsrcDef({'a': '4alpha'}, ['A', 'B']),
+    'D': RsrcDef({'c': GetRes('C')}, []),
+})
+engine.update_stack('foo', example_template2)
+engine.noop(9)
+engine.call(verify, example_template2)
---
+++ scenarios/update_replace_missed_cleanup.py
@@ -0,0 +1,44 @@
+def check_resource_count(expected_count):
+    test.assertEqual(expected_count, len(reality.all_resources()))
+
+def check_c_count(expected_count):
+    test.assertEqual(expected_count,
+                     len(reality.resources_by_logical_name('C')))
+
+example_template = Template({
+    'A': RsrcDef({'a': 'initial'}, []),
+    'B': RsrcDef({}, []),
+    'C': RsrcDef({'!a': GetAtt('A', 'a')}, ['B']),
+    'D': RsrcDef({'c': GetRes('C')}, []),
+    'E': RsrcDef({'ca': GetAtt('C', '!a')}, []),
+})
+engine.create_stack('foo', example_template)
+engine.noop(5)
+engine.call(verify, example_template)
+
+example_template_shrunk = Template({
+    'A': RsrcDef({'a': 'updated'}, []),
+    'B': RsrcDef({}, []),
+    'C': RsrcDef({'!a': GetAtt('A', 'a')}, ['B']),
+    'D': RsrcDef({'c': GetRes('C')}, []),
+    'E': RsrcDef({'ca': GetAtt('C', '!a')}, []),
+})
+engine.update_stack('foo', example_template_shrunk)
+engine.noop(5)
+
+example_template_long = Template({
+    'A': RsrcDef({'a': 'updated'}, []),
+    'B': RsrcDef({}, []),
+    'C': RsrcDef({'!a': GetAtt('A', 'a')}, ['B']),
+    'D': RsrcDef({'c': GetRes('C')}, []),
+    'E': RsrcDef({'ca': GetAtt('C', '!a')}, []),
+    'F': RsrcDef({}, ['D', 'E']),
+})
+engine.update_stack('foo', example_template_long)
+engine.call(check_c_count, 2)
+engine.noop(11)
+engine.call(verify, example_template_long)
+
+engine.delete_stack('foo')
+engine.noop(12)
+engine.call(verify, Template({}))
