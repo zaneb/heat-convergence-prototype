7fca127c92932acd0b84ee5ddb30acec1d471cf2
Michal Dulko <michal.dulko@intel.com>
2014-11-25 12:36:05 +0100

    Adding test case for update-replace of undependent node.

--- scenarios/update_replace_independent.py
+++ scenarios/update_replace_independent.py
@@ -49,15 +49,7 @@ def validate_update():
     res_ds = get_datastore("Resource")
     test.testcase.assertEqual(
         dict(res_ds.dump()),
-        {0: {'key': 0,
-             'name': 'A',
-             'phys_id': 'A_phys_id',
-             'properties': {'a': 'updated'},
-             'state': 'COMPLETE',
-             'version': 2,
-             'prop_refs': [],
-             },
-         1: {'key': 1,
+        {1: {'key': 1,
              'name': 'B',
              'phys_id': 'B_phys_id',
              'properties': {},
@@ -65,31 +57,38 @@ def validate_update():
              'version': 1,
              'prop_refs': [],
              },
+         2: {'key': 2,
+             'name': 'C',
+             'phys_id': 'C_phys_id',
+             'properties': {'!a': 'updated'},
+             'state': 'COMPLETE',
+             'version': 2,
+             'prop_refs': [{'resource': 'A2', 'version': 1}],
+             },
          3: {'key': 3,
              'name': 'E',
              'phys_id': 'E_phys_id',
              'properties': {'ca': 'updated'},
              'state': 'COMPLETE',
-             'version': 2,
-             'prop_refs': [{'resource': 'C2', 'version': 1}],
+             'version': 1,
+             'prop_refs': [{'resource': 'C', 'version': 2}],
              },
          4: {'key': 4,
              'name': 'D',
              'phys_id': 'D_phys_id',
-             'properties': {'c': {'name': 'C2', 'phys_id': 'C2_phys_id'}},
+             'properties': {'c': {'name': 'C', 'phys_id': 'C_phys_id'}},
              'state': 'COMPLETE',
-             'version': 2,
+             'version': 1,
              'prop_refs': [],
              },
          5: {'key': 5,
-             'name': 'C2',
-             'phys_id': 'C2_phys_id',
-             'properties': {'!a': 'updated'},
+             'name': 'A2',
+             'phys_id': 'A2_phys_id',
+             'properties': {'a': 'updated'},
              'state': 'COMPLETE',
-             'version': 1,
-             'prop_refs': [{'resource': 'A', 'version': 2}],
-             },
-         }
+             'version': 2,
+             'prop_refs': [],
+             },}
     )
 
 
@@ -105,11 +104,11 @@ engine.converge('update_replace', 10)
 engine.validate(validate_create)
 
 example_template_replaced = Template({
-    'A': RsrcDef({'a': 'updated'}, []),
+    'A2': RsrcDef({'a': 'updated'}, []),
     'B': RsrcDef({}, []),
-    'C2': RsrcDef({'!a': GetAtt('A', 'a')}, ['B', 'A']),
-    'D': RsrcDef({'c': GetRes('C2')}, ['C2']),
-    'E': RsrcDef({'ca': GetAtt('C2', '!a')}, ['C2']),
+    'C': RsrcDef({'!a': GetAtt('A2', 'a')}, ['B', 'A2']),
+    'D': RsrcDef({'c': GetRes('C')}, ['C']),
+    'E': RsrcDef({'ca': GetAtt('C', '!a')}, ['C']),
 })
 engine.update_stack('update_replace', example_template_replaced)
 engine.converge('update_replace', 10)
