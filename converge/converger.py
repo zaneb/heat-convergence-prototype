744730425a737351cc2100b91cf6c44bf1f27ccb
Zane Bitter <zbitter@redhat.com>
2014-12-03 16:51:53 -0500

    Refactor SyncPoints into a separate file

--- converge/converger.py
+++ converge/converger.py
@@ -4,13 +4,11 @@ from .framework import datastore
 from .framework import process
 
 from . import resource
+from . import sync_point
 from . import template
 
 
 logger = logging.getLogger('converger')
-
-sync_points = datastore.Datastore('SyncPoint',
-                                  'key', 'predecessors', 'satisfied')
 
 
 class Converger(process.MessageProcessor):
@@ -93,66 +91,19 @@ class Converger(process.MessageProcessor):
 
         key = '%s-%s' % (stack.key, template_key)
 
-        try:
-            sync_point = sync_points.read(key)
-        except sync_points.NotFound:
-            sync_points.create_with_key(key, predecessors=roots,
-                                        satisfied={sender: None})
-        else:
-            satisfied = dict(sync_point.satisfied)
-            satisfied[sender] = None
-            if set(satisfied).issuperset(roots):
-                stack.mark_complete(template_key)
-                sync_points.delete(key)
-            else:
-                sync_points.update(key, predecessors=roots,
-                                   satisfied=satisfied)
+        def mark_complete(stack_key, data):
+            stack.mark_complete(template_key)
+
+        sync_point.sync(key, mark_complete, stack.key, roots, {sender: None})
 
     def propagate_check_resource(self, next_res_graph_key, template_key,
                                  predecessors, sender, sender_data,
                                  deps, forward):
-        if len(predecessors) == 1:
-            logger.debug('[%s] Immediate %s %s: %s == %s',
-                         template_key,
-                         next_res_graph_key,
-                         'update' if forward else 'cleanup',
-                         sender, predecessors)
-            # Cut to the chase
-            self.check_resource(next_res_graph_key, template_key,
-                                {sender: sender_data}, deps, forward)
-            return
-
         key = '%s-%s-%s' % (next_res_graph_key.key, template_key,
                             'update' if forward else 'cleanup')
-        try:
-            sync_point = sync_points.read(key)
-        except sync_points.NotFound:
-            logger.debug('[%s] Waiting %s %s: %s != %s',
-                         template_key,
-                         next_res_graph_key,
-                         'update' if forward else 'cleanup',
-                         sender, predecessors)
-            sync_points.create_with_key(key, predecessors=predecessors,
-                                        satisfied={sender: sender_data})
-        else:
-            satisfied = dict(sync_point.satisfied)
-            satisfied[sender] = sender_data
-            predecessors |= sync_point.predecessors
-            if set(satisfied).issuperset(predecessors):
-                logger.debug('[%s] Ready %s %s: %s == %s',
-                             template_key,
-                             next_res_graph_key,
-                             'update' if forward else 'cleanup',
-                             set(satisfied), predecessors)
-                self.check_resource(next_res_graph_key, template_key,
-                                    satisfied, deps, forward)
-                sync_points.delete(key)
-            else:
-                # Note: update must be atomic
-                logger.debug('[%s] Waiting %s %s: %s != %s',
-                             template_key,
-                             next_res_graph_key,
-                             'update' if forward else 'cleanup',
-                             set(satisfied), predecessors)
-                sync_points.update(key, predecessors=predecessors,
-                                   satisfied=satisfied)
+
+        def do_check(target_key, data):
+            self.check_resource(target_key, template_key, data, deps, forward)
+
+        sync_point.sync(key, do_check, next_res_graph_key, predecessors,
+                        {sender: sender_data})
---
+++ converge/sync_point.py
@@ -0,0 +1,41 @@
+import logging
+
+from .framework import datastore
+
+
+logger = logging.getLogger('sync_point')
+
+sync_points = datastore.Datastore('SyncPoint',
+                                  'key', 'predecessors', 'satisfied')
+
+
+def sync(key, propagate, target, predecessors, new_data):
+    if set(new_data).issuperset(predecessors):
+        logger.debug('[%s] Immediate %s: %s == %s',
+                     key, target, set(new_data), predecessors)
+        # Cut to the chase
+        propagate(target, new_data)
+        return
+
+    try:
+        sync_point = sync_points.read(key)
+    except sync_points.NotFound:
+        logger.debug('[%s] Waiting %s: %s != %s',
+                     key, target, set(new_data), predecessors)
+        sync_points.create_with_key(key, predecessors=predecessors,
+                                    satisfied=new_data)
+    else:
+        satisfied = dict(sync_point.satisfied)
+        satisfied.update(new_data)
+        predecessors |= sync_point.predecessors
+        if set(satisfied).issuperset(predecessors):
+            logger.debug('[%s] Ready %s: %s == %s',
+                         key, target, set(satisfied), predecessors)
+            propagate(target, satisfied)
+            sync_points.delete(key)
+        else:
+            # Note: update must be atomic
+            logger.debug('[%s] Waiting s %s: %s != %s',
+                         key, target, set(satisfied), predecessors)
+            sync_points.update(key, predecessors=predecessors,
+                               satisfied=satisfied)
